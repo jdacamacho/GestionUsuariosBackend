833ee47731a22f5afcca0608605a3eac8a7c4c6f
Julian Camacho <jdacamacho@unicauca.edu.co>
2023-12-22 13:28:25 -0500

    protecciode endpoints

--- src/main/java/com/unicauca/gestion/Infrastucture/Configuration/SecurityConfig.java
+++ src/main/java/com/unicauca/gestion/Infrastucture/Configuration/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.unicauca.gestion.Infrastucture.Configuration;
 
+import org.modelmapper.internal.bytebuddy.asm.Advice.Return;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+        .csrf(csrf ->
+            csrf.disable())
+        .authorizeHttpRequests(authRequest ->
+            authRequest
+                .requestMatchers("/apiStudent/students/auth").permitAll()
+                .requestMatchers("/apiProfessor/professors/auth").permitAll()
+                .anyRequest().authenticated()
+                )
+            .build();
+    }
+
 }
